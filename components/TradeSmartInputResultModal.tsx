fa8792e2d691322cf6a2ff836c373b8ae451f60a
lujian213 <lujian213@msn.com>
2026-05-06 23:46:37 +0800

    bugfix: fix the OCR result windows scroll bar issue

--- components/TradeSmartInputResultModal.tsx
+++ components/TradeSmartInputResultModal.tsx
@@ -340,7 +340,7 @@ export function TradeSmartInputResultModal({
           </button>
         </div>
 
-        <div className="px-5 py-4 flex-shrink-0">
+        <div className="px-5 py-4 flex-1 min-h-0 overflow-y-auto">
           <div className="border border-gray-100 rounded-xl overflow-hidden" style={{ height: '350px' }}>
             <div className="h-full overflow-y-auto" style={{ scrollbarGutter: 'stable' }}>
               <table className="w-full text-sm">
